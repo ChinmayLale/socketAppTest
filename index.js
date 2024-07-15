433642835f02de41b297bccbac05ed1296c7bccc
ChinmayLale <chinmay29.lale@gmail.com>
2024-07-15 22:49:08 +0530

    Good To GO For Deploy

---
+++ index.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const { Server } = require("socket.io")
+const cors = require('cors');
+
+const app = express();
+app.use(express.static(path.resolve("./public")))
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*", // Your frontend URL
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+
+
+
+//socket io connections
+var msga = ""
+io.on('connection', (client) => {
+    console.log('a user connected to server : ', client.id);
+
+    client.on("disconnect", () => {
+        console.log("User Disconnected : ", client.id);
+        client.disconnect(true);
+    })
+
+    client.on('join-room',(msg)=>{
+        console.log("join room : ",msg);
+        client.join('chinmay');
+    })
+
+    client.on('user-msg', (data) => {
+        console.log("User Msg Revived --> ", data)
+        client.emit('replay','Waiting for chinmay to join chat');
+        client.join('chinmay')
+        client.to('chinmay').emit('user-msg',data);
+    })
+
+    client.on('chinmay-msg',(msg)=>{
+        console.log("chinmay msg --> ",msg);
+        client.to('chinmay').emit('chinmay-msg',msg);
+    })
+   
+
+});
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile("/public/index.html")
+})
+
+
+server.listen(8000, () => {
+    console.log("Server Started At Port 8000")
+})
+
