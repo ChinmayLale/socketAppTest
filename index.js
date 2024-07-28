4bcaf656f0b81a874253a40dc7781e2100ab686b
ChinmayLale <chinmay29.lale@gmail.com>
2024-07-28 15:09:26 +0530

    Added Pooling Check

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const TelegramBot = require('node-telegram-bot-api');
 
 
 const token = '7352657896:AAGTchr9t90beofDgT5Dg_XZFQ7VcVVN1xU';
-const bot = new TelegramBot(token, { polling: true });
+
 const YOUR_CHAT_ID = '5110477383';
 
-
+let bot ;
 const app = express();
 app.use(express.json()); 
 app.use(express.static(path.resolve("./public")))
@@ -32,15 +32,45 @@ const io = new Server(server, {
 
 
 
+function gracefulShutdown() {
+    console.log('Shutting down gracefully...');
+    if (bot) {
+        bot.stopPolling();
+    }
+}
+
+function initBot() {
+    if (bot) {
+        bot.stopPolling();
+    }
+    bot = new TelegramBot(token, { polling: true });
+    bot.on('polling_error', (error) => {
+        console.error('Polling error:');
+        // Wait for a bit before trying to reconnect
+        setTimeout(initBot, 5000);
+    });
+
+    bot.on('message', (msg) => {
+        const chatId = msg.chat.id;
+        if (chatId.toString() === YOUR_CHAT_ID) {
+            console.log('Received message from Telegram:', msg.text);
+            // Broadcast the message to all connected clients
+            io.to('chinmay').emit('chinmay-msg', msg.text);
+        }
+    });
+}
+
 
 //socket io connections routes
 var msga = ""
 io.on('connection', (client) => {
     console.log('a user connected to server : ', client.id);
-
+    
     client.on("disconnect", () => {
         console.log("User Disconnected : ", client.id);
+        gracefulShutdown();
         client.disconnect(true);
+
     })
 
     client.on('join-room',(msg)=>{
@@ -49,6 +79,7 @@ io.on('connection', (client) => {
     })
     let flag = false;
     client.on('user-msg', (data) => {
+        initBot();
         console.log("User Msg Revived --> ", data)
         if (!flag) {
         client.emit('replay','Waiting for chinmay to join chat');
@@ -71,14 +102,9 @@ io.on('connection', (client) => {
 
 });
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    if (chatId.toString() === YOUR_CHAT_ID) {
-        console.log('Received message from Telegram:', msg.text);
-        // Broadcast the message to all connected clients
-        io.to('chinmay').emit('chinmay-msg', msg.text);
-    }
-});
+
+
+// const bot = new TelegramBot(token, { polling: true });
 
 
 
