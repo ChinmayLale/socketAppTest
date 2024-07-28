6169affe48f6485e03507fbebda08f9818f0f024
ChinmayLale <chinmay29.lale@gmail.com>
2024-07-28 13:19:08 +0530

    Added Telegram Bot

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const path = require('path');
 const express = require('express');
 const { Server } = require("socket.io")
 const cors = require('cors');
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const token = '7352657896:AAGTchr9t90beofDgT5Dg_XZFQ7VcVVN1xU';
+const bot = new TelegramBot(token, { polling: true });
+const YOUR_CHAT_ID = '5110477383';
+
 
 const app = express();
+app.use(express.json()); 
 app.use(express.static(path.resolve("./public")))
 app.use(cors({
     origin: '*',
@@ -39,11 +47,19 @@ io.on('connection', (client) => {
         console.log("join room : ",msg);
         client.join('chinmay');
     })
-
+    let flag = false;
     client.on('user-msg', (data) => {
         console.log("User Msg Revived --> ", data)
+        if (!flag) {
         client.emit('replay','Waiting for chinmay to join chat');
-        client.join('chinmay')
+        flag=true;
+        }
+        client.join('chinmay');
+        bot.sendMessage(YOUR_CHAT_ID, data).then(() => {
+            console.log('Message sent to Telegram');
+        }).catch((error) => {
+            console.error('Error sending message to Telegram:', error);
+        });
         client.to('chinmay').emit('user-msg',data);
     })
 
@@ -55,15 +71,24 @@ io.on('connection', (client) => {
 
 });
 
-
-
-
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (chatId.toString() === YOUR_CHAT_ID) {
+        console.log('Received message from Telegram:', msg.text);
+        // Broadcast the message to all connected clients
+        io.to('chinmay').emit('chinmay-msg', msg.text);
+    }
+});
 
 
 
 app.get('/', (req, res) => {
     res.sendFile("/public/index.html")
 })
+
+
+
+
 
 
 server.listen(8000, () => {
